13501b20496a10d448d346802d8debd695a63210
Wojan93 <wojciechpolubiec@gmail.com>
2020-02-12 21:56:02 +0100

    applied the first commit from branch afterLombok

--- OBRONA/1moto/src/main/java/com/example/java/moto/model/Graphics.java
+++
@@ -1,9 +0,0 @@
-package com.example.java.moto.model;
-
-import javax.persistence.Entity;
-
-@Entity
-public class Graphics extends BaseEntity {
-
-    private String fileName;
-}
--- OBRONA/1moto/src/main/java/com/example/java/moto/repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.java.moto.repository;
-
-import com.example.java.moto.model.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarRepository extends JpaRepository<Car, String> {
-}
